8297d232d5c9c7b7a42bdd0f0700f70aa57710c4
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2018-10-07 17:50:35 +0300

    connect the actions inside the controller\ rename some actions

--- Application/Controller.py
+++ Application/Controller.py
@@ -1,4 +1,5 @@
 from Application.View import MagnifierWindow, MainWindow, PlotterWindow
+from PyQt5 import QtCore
 
 
 class Controller(object):
@@ -8,5 +9,41 @@ class Controller(object):
         self.plotterWindow = PlotterWindow(self.mainWindow)
         self.magnifierWindow = MagnifierWindow(self.mainWindow)
 
+        # connect the actions to methods
+        self.mainWindow.actionExit.triggered.connect(self.actionExit)
+        self.mainWindow.actionInvert.triggered.connect(self.actionInvert)
+        self.mainWindow.actionLoadColorImage.triggered.connect(self.actionLoadColorImage)
+        self.mainWindow.actionLoadGrayscaleImage.triggered.connect(self.actionLoadColorImage)
+        self.mainWindow.actionMagnifier.triggered.connect(self.actionMagnifier)
+        self.mainWindow.actionPlotter.triggered.connect(self.actionPlotter)
+        self.mainWindow.actionSaveProcessedImage.triggered.connect(self.actionSaveProcessedImage)
+
         # show the main window
         self.mainWindow.show()
+
+    def actionExit(self):
+        QtCore.QCoreApplication.quit()
+
+    def actionInvert(self):
+        # TODO
+        pass
+
+    def actionLoadColorImage(self):
+        # TODO
+        pass
+
+    def actionLoadGrayscaleImage(self):
+        # TODO
+        pass
+
+    def actionMagnifier(self):
+        # TODO
+        self.magnifierWindow.show()
+
+    def actionPlotter(self):
+        # TODO
+        self.plotterWindow.show()
+
+    def actionSaveProcessedImage(self):
+        # TODO
+        pass
