a24ffa2849864b99c36462c595c78209a3e56ffa
Vlad Vrabie <vladvrabie97@gmail.com>
2018-10-28 01:26:00 +0300

    made settings read-only

--- Application/Settings.py
+++ Application/Settings.py
@@ -1,20 +1,98 @@
 from enum import Enum
 
-# TODO: try to make those readonly
+
 class MainWindowSettings:
-    zoomMinimumValue = 0.0
-    zoomMaximumValue = 20.0
-    zoomSingleStep = 0.05
-    zoomPageStep = 1.0
-    zoomDefaultValue = 1.0
-    ticksInterval = 1
+
+    @property
+    def zoomMinimumValue(self):
+        """
+        TODO: document MainWindowSettings.zoomMinimumValue
+        :return:
+        """
+        return 0.0
+
+    @property
+    def zoomMaximumValue(self):
+        """
+        TODO: document MainWindowSettings.zoomMaximumValue
+        :return:
+        """
+        return 20.0
+
+    @property
+    def zoomSingleStep(self):
+        """
+        TODO: document MainWindowSettings.zoomSingleStep
+        :return:
+        """
+        return 0.05
+
+    @property
+    def zoomPageStep(self):
+        """
+        TODO: document MainWindowSettings.zoomPageStep
+        :return:
+        """
+        return 1.0
+
+    @property
+    def zoomDefaultValue(self):
+        """
+        TODO: document MainWindowSettings.zoomDefaultValue
+        :return:
+        """
+        return 1.0
+
+    @property
+    def ticksInterval(self):
+        """
+        TODO: document MainWindowSettings.ticksInterval
+        :return:
+        """
+        return 1
+
+
+# had to make it singleton so that the properties might call themselves correctly
+MainWindowSettings = MainWindowSettings()
 
 
 class MagnifierWindowSettings:
-    frameGridSize = 9  # positive odd number here
-    threeZoneHeightPadding = 12  # in pixels
-    fourZoneHeightPadding = 6  # in pixels
-    fontSize = 8  # in points
+
+    @property
+    def frameGridSize(self):
+        return 9
+
+    @frameGridSize.getter
+    def frameGridSize(self):
+        """
+        TODO document MagnifierWindowSettings.frameGridSize
+        :return:
+        """
+        return 9  # positive odd number here
+
+    @property
+    def threeZoneHeightPadding(self):
+        """
+        TODO document MagnifierWindowSettings.threeZoneHeightPadding
+        :return:
+        """
+        return 12  # in pixels
+
+    @property
+    def fourZoneHeightPadding(self):
+        """
+        TODO document MagnifierWindowSettings.fourZoneHeightPadding
+        :return:
+        """
+        return 6  # in pixels
+
+    @property
+    def fontSize(self):
+        """
+        TODO document MagnifierWindowSettings.fontSize
+        :return:
+        """
+        return 8  # in points
 
     class ColorSpaces(Enum):
         RGB = (0, 'RGB (Red Green Blue)')
@@ -22,6 +100,10 @@ class MagnifierWindowSettings:
         HSV = (2, 'HSV (Hue Saturation Value)')
         CMYK = (3, 'CMYK (Cyan Magenta Yellow Black)')
         GRAY = (4, 'Grayscale')
+
+
+# had to make it singleton so that the properties might call themselves correctly
+MagnifierWindowSettings = MagnifierWindowSettings()
 
 
 class PlotterWindowSettings:
