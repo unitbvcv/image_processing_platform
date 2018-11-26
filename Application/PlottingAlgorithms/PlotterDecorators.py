f45a5f4a81972e0226519db1a62cf103b8a7a525
Vlad Vrabie <vladvrabie97@gmail.com>
2018-11-26 13:38:58 +0200

    implemented plotterfunction decorator

---
+++ Application/PlottingAlgorithms/PlotterDecorators.py
@@ -0,0 +1,40 @@
+import functools
+from Application.PlottingAlgorithms import registeredAlgorithms
+
+
+class PlotterFunction(object):
+
+    def __init__(self, title, fromMainModel=[], computeOnImageChanged=False, computeOnClick=False):
+        self._title = title
+        self._fromMainModel = fromMainModel
+        self._computeOnImageChanged = computeOnImageChanged
+        self._computeOnClick = computeOnClick
+
+    def __call__(self, func):
+
+        class PlotterFunctionWrapper:
+            def __init__(self, func, argList, onChange, onClick):
+                self._argNames = argList
+                self._func = func
+                self._computeOnImageChanged = onChange
+                self._computeOnClick = onClick
+
+                functools.update_wrapper(self, func)
+
+            @property
+            def computeOnImageChanged(self):
+                return self._computeOnImageChanged
+
+            @property
+            def computeOnClick(self):
+                return self._computeOnClick
+
+            def __call__(self, *args, **kwargs):
+                return self._func(*args, **kwargs)
+
+            def prepare(self, mainModel):
+                return {argName: mainModel.__dict__[argName] for argName in self._argNames}
+
+        wrapper = PlotterFunctionWrapper(func, self._fromMainModel, self._computeOnImageChanged, self._computeOnClick)
+        registeredAlgorithms[self._title] = wrapper
+        return wrapper
