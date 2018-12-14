c1963e1c8b8dcca89077fc578a45691e2efb6a21
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2018-12-14 19:33:42 +0200

    create a signal for mainvm to catch when pressing load ... image

--- Application/ViewModels/MainWindowVM.py
+++ Application/ViewModels/MainWindowVM.py
@@ -3,9 +3,12 @@ from Application.Views.MainWindowView import MainWindowView
 from PyQt5 import QtCore, QtWidgets
 import Application.Settings
 import cv2 as opencv
+from PyQt5.QtCore import pyqtSignal
 
 
 class MainWindowVM(QtCore.QObject):
+    loadImageSignal = pyqtSignal(str, bool, name="loadImageSignal")
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -17,3 +20,23 @@ class MainWindowVM(QtCore.QObject):
 
         # show the main window
         self._view.show()
+
+        # connect actions signals to slots
+        self._view.actionLoadGrayscaleImage.triggered.connect(self._actionLoadGrayscaleImage)
+        self._view.actionLoadColorImage.triggered.connect(self._actionLoadColorImage)
+
+    def _actionLoadGreyscaleImage(self):
+        filePath, _ = QtWidgets.QFileDialog.getOpenFileName(parent=self._view, caption='Open grayscale file',
+                                                            filter='Image files (*.bmp *.dib *.jpeg *.jpg *.jpe *.jp2 '
+                                                                   '*.png *.webp *.pbm *.pgm *.ppm *.ras *.sr *.tiff *.tif)'
+                                                            )
+
+        self.loadImageSignal.emit(filePath, True)
+
+    def _actionLoadColorImage(self):
+        filePath, _ = QtWidgets.QFileDialog.getOpenFileName(parent=self._view, caption='Open color file',
+                                                            filter='Image files (*.bmp *.dib *.jpeg *.jpg *.jpe *.jp2 '
+                                                                   '*.png *.webp *.pbm *.pgm *.ppm *.ras *.sr *.tiff *.tif)'
+                                                            )
+
+        self.loadImageSignal.emit(filePath, False)
